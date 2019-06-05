b5d4c2a6dc4429330367e225ef2d169f7d19f88d
RonWuYi <lyshmily@outlook.com>
2019-06-05 02:06:16 -0700

    update selenium func

--- test002.py
+++ test002.py
@@ -1,4 +1,6 @@
 import time
+import pyautogui
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 # from selenium.common.exceptions import TimeoutException
@@ -6,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
+driver.maximize_window()
 
 # driver.get("https://www.google.com")
 driver.get("https://pokeassistant.com/main/ivcalculator")
@@ -30,6 +33,16 @@ time.sleep(1)
 
 search_hp = driver.find_element_by_name("search_hp")
 
+xy = pokemon_name.location
+print(xy.keys())
+print(xy.values())
+
+print(xy['x'])
+print(type(xy['x']))
+print(xy['y'])
+print(type(xy['y']))
+
+x, y = xy['x'], xy['y']
 search_hp.send_keys("80")
 
 time.sleep(1)
@@ -49,12 +62,19 @@ time.sleep(1)
 #
 # search_dust.send_keys("1600")
 #
+# pyautogui.click()
 try:
     inputElement = driver.find_element_by_id("calculatebtn")
 except Exception as e:
     print(e)
-
+time.sleep(1)
+pyautogui.click(x + 75, y + 91)
+time.sleep(1)
+pyautogui.moveTo(x + 126, y + 172, 0.5)
+pyautogui.click(x + 126, y + 172)
+time.sleep(1)
 inputElement.click()
+time.sleep(1)
 # inputElement.submit()
 
 # try:
@@ -63,3 +83,5 @@ inputElement.click()
 #
 # finally:
 #     driver.quit()
+
+# driver.quit()
