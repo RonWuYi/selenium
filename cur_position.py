beb0bcfafeb93c82633afd2609294d52d4324098
RonWuYi <lyshmily@outlook.com>
2019-06-05 01:10:40 -0700

    update selenium func

---
+++ cur_position.py
@@ -0,0 +1,13 @@
+import os, time
+import pyautogui as pya
+
+
+while True:
+    x, y = pya.position()
+    pos = "Position: {} , {}".format(str(x).rjust(4), str(y).rjust(4))
+
+    print(pos)
+    time.sleep(0.3)
+    os.system('clear')
+
+# print('ending...')
