24b2215375bee292e2b44b256515e747191d46a9
RonWuYi <lyshmily@outlook.com>
2019-06-05 00:39:30 -0700

    update selenium func

--- test002.py
+++ test002.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 # from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -23,19 +24,21 @@ time.sleep(1)
 
 search_cp = driver.find_element_by_name("search_cp")
 
-search_cp.send_keys("80")
+search_cp.send_keys("503")
 
 time.sleep(1)
 
 search_hp = driver.find_element_by_name("search_hp")
 
-search_hp.send_keys("1600")
+search_hp.send_keys("80")
 
 time.sleep(1)
 
-search_dust = driver.find_element_by_name("search_dust")
+search_dust = Select(driver.find_element_by_name("search_dust"))
 
-search_dust.send_keys("1600")
+# search_dust = driver.find_element_by_name("search_dust")
+
+search_dust.select_by_value("1600")
 
 time.sleep(1)
 # search_dust = driver.find_element_by_name("search_dust")
@@ -46,11 +49,17 @@ time.sleep(1)
 #
 # search_dust.send_keys("1600")
 #
+try:
+    inputElement = driver.find_element_by_id("calculatebtn")
+except Exception as e:
+    print(e)
+
+inputElement.click()
 # inputElement.submit()
 
-try:
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-    print(driver.title)
-
-finally:
-    driver.quit()
+# try:
+#     WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+#     print(driver.title)
+#
+# finally:
+#     driver.quit()
